d3a6802b8453a0a7f247dcf3f02a6ce2444f5791
Sabasahban <sabaasahban@gmail.com>
2023-01-26 14:43:11 +0330

    fix errors

--- xv6-labs-2022/kernel/defs.h
+++ xv6-labs-2022/kernel/defs.h
@@ -10,6 +10,7 @@ struct spinlock;
 struct sleeplock;
 struct stat;
 struct superblock;
+struct pstat;
 
 // bio.c
 void            binit(void);
@@ -108,7 +109,8 @@ void            yield(void);
 int             either_copyout(int user_dst, uint64 dst, void *src, uint64 len);
 int             either_copyin(void *dst, int user_src, uint64 src, uint64 len);
 void            procdump(void);
-int             getpinfo(struct pstat* ps);
+int             settickets(int);
+int             getpinfo(struct pstat*);
 
 // swtch.S
 void            swtch(struct context*, struct context*);
